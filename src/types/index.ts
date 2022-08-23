43113321be13649a91e3ae4c3cd0ed99199518f6
Ranjit Kairi <ranjitkoiri009@gmail.com>
2022-08-23 17:28:12 +0530

    fea(): add format on progress

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,33 @@
-export interface IRowClientData {
-  id: number
-  avatar?: string
-  name: string
-  email: string
-  city: string
-  state: string
-  edit?: string
-  delete?: string
-}
+import { z } from 'zod'
+
+// Validation
+const zTutorial = z.object({
+  id: z.any().optional(),
+  avatar: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+  city: z.string(),
+  state: z.string(),
+  edit: z.string(),
+  delete: z.string(),
+  published: z.boolean().default(false),
+})
+
+// Type Definition
+type IRowClientData = z.infer<typeof zTutorial>
+
+export type { IRowClientData }
+
+// export interface IRowClientData {
+//   id: number
+//   avatar?: string
+//   name: string
+//   email: string
+//   city: string
+//   state: string
+//   edit?: string
+//   delete?: string
+// }
 
 export interface IRowVendorData {
   id: number
