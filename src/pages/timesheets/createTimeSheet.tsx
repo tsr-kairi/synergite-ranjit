aa191bdb19b38365802ce91fe6df0caec1dcdef6
Raja <115618618+RajaSynergy@users.noreply.github.com>
2022-10-19 16:21:38 +0530

    code formatted

--- src/pages/timesheets/createTimeSheet.tsx
+++ src/pages/timesheets/createTimeSheet.tsx
@@ -1,5 +1,13 @@
 import { ListViewLayout } from '@/components/layout/list-view.layout'
-import { Button, Checkbox, createStyles, Select, Table, Text, TextInput } from '@mantine/core'
+import {
+  Button,
+  Checkbox,
+  createStyles,
+  Select,
+  Table,
+  Text,
+  TextInput,
+} from '@mantine/core'
 import { Th } from '../employee/employee-list'
 import React, { useState } from 'react'
 import { IconPlus, IconSubmarine } from '@tabler/icons'
@@ -24,66 +32,57 @@ const CreateTimeSheet = () => {
   const useStyles = createStyles((theme) => ({
     th: {
       paddingLeft: '20px !important',
-     
-
     },
     td: {
       padding: '20px !important',
     },
-    tr:{
+    tr: {
       display: 'flex',
       flexDirection: 'row',
-      gap:'130px',
-      marginLeft:'72px'
+      gap: '130px',
+      marginLeft: '72px',
     },
-    tbody:{
-      gap:'120px',
+    tbody: {
+      gap: '120px',
       display: 'flex',
       flexDirection: 'row',
-      marginLeft:'60px'
-
-
+      marginLeft: '60px',
     },
-    buttonContainer:{
-      position:'absolute',
-      top:'180px',
-      right:'180px'
+    buttonContainer: {
+      position: 'absolute',
+      top: '180px',
+      right: '180px',
     },
-    iconContainer:{
-      position:'absolute',
-      top:'180px',
-      right:'150px'
-
-
-    }
-  
-     }))
-     const {classes} = useStyles()
+    iconContainer: {
+      position: 'absolute',
+      top: '180px',
+      right: '150px',
+    },
+  }))
+  const { classes } = useStyles()
   return (
     <>
-      <tr
-        className={classes.tr}
-      >
+      <tr className={classes.tr}>
         <Th onSort={() => null}>Date</Th>
         <Th onSort={() => null}>Project</Th>
         <Th onSort={() => null}>Billable/Not</Th>
         <Th onSort={() => null}>Total Hrs</Th>
       </tr>
 
-      <Table  horizontalSpacing="md" verticalSpacing="xs">
-        <thead className={classes.th} >
+      <Table horizontalSpacing="md" verticalSpacing="xs">
+        <thead className={classes.th}>
           <>
             {fields.map((field, idx) => {
               return (
                 <>
                   <tbody className={classes.tbody} key={`${field}-${idx}`}>
-                    <td >
-                    <td>19/10/2022</td>
+                    <td>
+                      <td>19/10/2022</td>
                     </td>
                     <td>
                       <TextInput placeholder="Project" withAsterisk />
                     </td>
-                    
+
                     <td>
                       <Checkbox label="" />
                     </td>
@@ -98,22 +97,16 @@ const CreateTimeSheet = () => {
                         data={['1', '2', '3', '4', '5', '6', '7', '8']}
                       />
                     </td>
-
-                    
-                    
                   </tbody>
                 </>
               )
             })}
           </>
-         
         </thead>
       </Table>
-      <Button
-        className={classes.buttonContainer} 
-         variant="white">
-      Submit
-    </Button>
+      <Button className={classes.buttonContainer} variant="white">
+        Submit
+      </Button>
       <div
         style={{
           display: 'flex',
@@ -121,21 +114,16 @@ const CreateTimeSheet = () => {
           flexDirection: 'row',
         }}
       >
-       <div>
-       <IconPlus
-          size={30}
-          strokeWidth={4}
-          color={'green'}
-          onClick={() => handleAdd()}
-          className={classes.iconContainer}
-
-        />
-        
-       </div>
-       
-    
+        <div>
+          <IconPlus
+            size={30}
+            strokeWidth={4}
+            color={'green'}
+            onClick={() => handleAdd()}
+            className={classes.iconContainer}
+          />
+        </div>
       </div>
-     
     </>
   )
 }
